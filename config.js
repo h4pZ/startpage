69d5429d64439624c3d018c8c24f4467b0e8447d
h4pZ <h4pz@protonmail.com>
2021-08-23 13:19:24 -0500

    custom startpage

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ var data = {
     // Clock config
 
     // Enable/disable clock
-    clock: true,
+    clock: false,
     // Foreground color of the clock
     clock_fgcolor: "#79acf3",
     // Font size of the clock
@@ -29,22 +29,22 @@ var data = {
     // Change image proprieties from here
 
     // Border radius: set it to 50% to create a circle
-    image_radius: "20px",
+    image_radius: "220px",
     // Border for the image
     image_border: "0px",
     // Color of the border
     image_bordercolor: "#79acf3",
     // Filters for the image.
-    image_filter: "grayscale(56%) brightness(60%)",
+	image_filter: "grayscale(10%) brightness(90%)",
     // Width/height for the image
     image_width: "300px",
     // Image source: you can use local files, or a link
-    // Ex.: image_source: "/img/kanji.jpg"
-    image_source: "https://raw.githubusercontent.com/Alededorigo/Kanji/main/img/kanji.jpg",
+    // Ex.: image_source: "/img/skull.jpg"
+    image_source: "https://pbs.twimg.com/profile_images/1376192964100288513/WEiQV9kE_400x400.jpg",
 
     // -------------------------------------------------
     // Enable/disable site icons
-    enable_icons: false,
+    enable_icons: true,
     links_hover: true,
 
     // -------------------------------------------------
@@ -54,7 +54,7 @@ var data = {
     // -------------------------------------------------
     // Change search bar proprietis from here
     // Search bar placeholder
-    search_placeholder: "Search here",
+    search_placeholder: "~",
     // Search engine
     // Possible choices: (case sensitive)
     // google
